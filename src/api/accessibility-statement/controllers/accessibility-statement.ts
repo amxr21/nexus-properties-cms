72ab1a5428bee08d5fc7ab2fbef2bc15de317109
amxr21 <ammar211080@gmail.com>
2026-06-09 08:46:53 +0400

    api: add accessibility statement content type

---
+++ src/api/accessibility-statement/controllers/accessibility-statement.ts
@@ -0,0 +1,2 @@
+import { factories } from '@strapi/strapi';
+export default factories.createCoreController('api::accessibility-statement.accessibility-statement');
---
+++ src/api/accessibility-statement/routes/accessibility-statement.ts
@@ -0,0 +1,2 @@
+import { factories } from '@strapi/strapi';
+export default factories.createCoreRouter('api::accessibility-statement.accessibility-statement');
